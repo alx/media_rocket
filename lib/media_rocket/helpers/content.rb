d2d76134b437f6efdf08e0f6c3b418d7ba04fa6e
Alexandre Girard <alx.girard@gmail.com>
2009-02-11 12:04:14 +0100

    change style on gallery edit form

--- lib/media_rocket/helpers/content.rb
+++ lib/media_rocket/helpers/content.rb
@@ -113,7 +113,7 @@ module MediaRocket
       def gallery_action_edit(gallery)
         link_to self_closing_tag(:img, :src => media_rocket_image_path("/icons/folder_edit.png"),
                                  :title => "Edit #{gallery.name}", :class => :icon),
-                url(:edit_media_rocket_gallery, :id => gallery.id) << "?height=150&width=320&modal=true",
+                url(:edit_media_rocket_gallery, :id => gallery.id) << "?height=350&width=350&modal=true",
                 :title => "Edit #{gallery.name}",
                 :class => :thickbox
       end
