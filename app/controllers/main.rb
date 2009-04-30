c82b862222591004a4403bb544e985270b3bba0b
Alexandre Girard <alx.girard@gmail.com>
2009-04-30 11:22:20 +0200

    review upload with filedata

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -11,36 +11,21 @@ class MediaRocket::Main < MediaRocket::Application
   
   def upload
     if params[:Filedata]
-      Merb.logger.info "filedata read"
       
       if @site = ::MediaRocket::Site.first(:id => params[:site_id])
       
         media_params = {:title => params[:Filename],
-                        :site_id => params[:site_id],
+                        :site_id => @site.id,
+                        :gallery_name => params[:gallery_name],
+                        :gallery_id => params[:gallery_id],
                         :file => {:filename => params[:Filename],
                                   :tempfile => params[:Filedata][:tempfile]}}
+                                  
         @media = ::MediaRocket::MediaFile.new(media_params)
-      
-        # Save media and insert it into gallery
-        if @media.save
+        @media.save
         
-          @site.reload
-        
-          if !params[:gallery_name].empty?
-            # gallery name specified, use it as media gallery
-            unless @gallery = ::MediaRocket::Gallery.first(:name => params[:gallery_name])
-              @gallery = @site.galleries.create :name => params[:gallery_name]
-            end
-          else
-            # use gallery from select box if no gallery specified
-            @gallery = ::MediaRocket::Gallery.first :id => params[:gallery_id]
-          end # !params[:gallery_name].empty?
-          
-          @gallery.medias << @media
-          @gallery.medias.reload
-          
-        end # @media.save
       end # @site = ...
+      
       # Return 1 for success in uploadify
       render "1", :layout => false
     else
