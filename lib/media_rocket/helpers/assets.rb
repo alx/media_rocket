26bd97ddf01fb88deabbcbb0a2269290018be7f9
Alexandre Girard <alx.girard@gmail.com>
2009-04-30 13:39:37 +0200

    add helper to build js/css list in html header

--- lib/media_rocket/helpers/assets.rb
+++ lib/media_rocket/helpers/assets.rb
@@ -26,6 +26,46 @@ module MediaRocket
       def media_rocket_public_path_for(type, *segments)
         ::MediaRocket.public_path_for(type, *segments)
       end
+      
+      def media_rocket_js
+        script = ""
+        %w{'jquery/jquery.js',
+           'jquery/jquery.ui.js',
+           'jquery/jquery.confirm.js',
+           'jquery/jquery.form.js',
+           'jquery/jquery.treetable.js',
+           'jquery/jquery.validate.js',
+           'jquery/jquery.livequery.js',
+           'jquery/thickbox.js',
+           'jquery/jquery.uploadify.js',
+           'json2.js',
+           'permissions.js',
+           'master.js'}.each do |file|
+          script << media_rocket_js_line(file)
+        end
+        script
+      end
+      
+      def media_rocket_js_line(file)
+        "<script src='#{media_rocket_javascript_path file}' type='text/javascript' charset='utf-8'></script>"
+      end
+      
+      def media_rocket_css
+        css = "<!--[if IE]>#{media_rocket_css_line 'ie.css'}<![endif]-->"
+      	
+        %w{'screen.css',
+           'print.css',
+           'jquery.treetable.css',
+           'thickbox.css',
+           'master.css'}.each do |file|
+          css << media_rocket_css_line(file)
+        end
+        css
+      end
+      
+      def media_rocket_css_line(file)
+        "<link rel='stylesheet' href='#{media_rocket_stylesheet_path file}' type='text/css' media='screen, projection'>"
+      end
     end
   end
 end
