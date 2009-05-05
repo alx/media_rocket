9f9257a52c61a819f49cc7681ff1626ba2fa2c53
Alexandre Girard <alx.girard@gmail.com>
2009-05-05 18:41:06 +0200

    correct syntax

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -14,10 +14,10 @@ class MediaRocket::Main < MediaRocket::Application
       
       if @site = ::MediaRocket::Site.first(:id => params[:site_id])
       
-        
+        gallery_name = params[:gallery_name].empty? "" : Base64.decode64(params[:gallery_name])
         media_params = {:title => params[:Filename],
                         :site_id => @site.id,
-                        :gallery_name => params[:gallery_name].empty? "" : Base64.decode64(params[:gallery_name]),
+                        :gallery_name => gallery_name,
                         :gallery_id => params[:gallery_id],
                         :file => {:filename => params[:Filename],
                                   :tempfile => params[:Filedata][:tempfile]}}
