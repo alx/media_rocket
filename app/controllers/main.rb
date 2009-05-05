d2aae2c44429de0b3cc82dcbfdc863e3f287ca75
Alexandre Girard <alx.girard@gmail.com>
2009-05-05 18:30:56 +0200

    resolve issue when no gallery name

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -14,9 +14,10 @@ class MediaRocket::Main < MediaRocket::Application
       
       if @site = ::MediaRocket::Site.first(:id => params[:site_id])
       
+        
         media_params = {:title => params[:Filename],
                         :site_id => @site.id,
-                        :gallery_name => Base64.decode64(params[:gallery_name]),
+                        :gallery_name => params[:gallery_name].empty? "" : Base64.decode64(params[:gallery_name]),
                         :gallery_id => params[:gallery_id],
                         :file => {:filename => params[:Filename],
                                   :tempfile => params[:Filedata][:tempfile]}}
