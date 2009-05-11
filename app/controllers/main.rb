8e9e081391652ae574c2b1c8fd81ef0eb71e3a3d
Alexandre Girard <alx.girard@gmail.com>
2009-05-11 14:49:26 +0200

    replace empty? by nil?

--- app/controllers/main.rb
+++ app/controllers/main.rb
@@ -14,7 +14,7 @@ class MediaRocket::Main < MediaRocket::Application
       
       if @site = ::MediaRocket::Site.first(:id => params[:site_id])
       
-        gallery_name = params[:gallery_name].empty? ? "" : Base64.decode64(params[:gallery_name])
+        gallery_name = params[:gallery_name].nil? ? "" : Base64.decode64(params[:gallery_name])
         media_params = {:title => params[:Filename],
                         :site_id => @site.id,
                         :gallery_name => gallery_name,
