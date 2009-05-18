e7780dd8a4c403aa81da13d4bf8f4bdfaf896a18
sebastien maurette <sebastienmaurette@macbook-pro-17-de-sebastien-maurette.local>
2009-05-18 18:47:36 +0200

    add delete button to gallery

--- lib/media_rocket/router.rb
+++ lib/media_rocket/router.rb
@@ -22,6 +22,7 @@ module MediaRocket
       scope.match('/upload').to(:controller => 'main', :action => 'upload').name(:upload).fixatable
 
       # Route to gallery xml
+      scope.match('/galleries/:action/:id').to(:controller => 'galleries', :action => 'destroy').name(:delete_galleries)
       scope.match('/galleries(.:format)').to(:controller => 'galleries', :action => 'list').name(:galleries)
       scope.match('/gallery/:id(.:format)').to(:controller => 'galleries', :action => 'gallery').name(:gallery)
       scope.match('/gallery_icon').to(:controller => 'galleries', :action => 'update').name(:gallery_icon)
