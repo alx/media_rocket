0586d6ebe523e303ae0cf1f93dcfeca744969bd7
Alexandre Girard <alx.girard@gmail.com>
2009-05-20 10:15:58 +0200

    refactor js

--- public/javascripts/master.js
+++ public/javascripts/master.js
@@ -1,55 +1,64 @@
 $(document).ready(function() {
 	
-	function display_add_gallery_name(gallery){
-		if (gallery){
-			// Display specific gallery, 
-			// set parent_id and gallery name for "add gallery" form
-			$('input#gallery-parent').attr('value', gallery.id);
-			$('input.gallery-id').attr('value', gallery.id);
-			
-			$('span#gallery-parent-name').parent('p').removeClass('hidden');
-			$('span#gallery-parent-name').text(gallery.name);
-			
-			// Display upload forms
-			if($('#action-upload-solo').attr('style') == "display: none;") {
-				$('#action-upload-solo').show('slide');
-				$('#action-upload-multi').show('slide');
-			}
-			
-		} else {
-			// User want to display main gallery
-			
-			// Hide upload forms
-			if($('#action-upload-solo').attr('style') != "display: none;") {
-				$('#action-upload-solo').hide('slide');
-				$('#action-upload-multi').hide('slide');
-			}
-			
-			// Update gallery id attributes
-			$('input#gallery-parent').attr('value', '');
-			$('input.gallery-id').attr('value', '');
-			
-			// Hide gallery name
-			$('span#gallery-parent-name').parent('p').addClass('hidden');
-		}
+// ========================================
+// Items manipulation
+	
+	// ----------------------------------------
+	// Create item from json source
+	
+	function load_item(type, item){
+		var loaded_item = "<div id='" + type + "-item-" + item.id + "' class='item " + type + "-item ui-corner-all'>";
+		loaded_item += "<p class='" + type + "-delete' id='" + type + "-item-delete-" + item.id + "'><a class='ui-icon ui-icon-circle-close'></a></p>"
+		loaded_item += "<img src='" + item.icon + "' /><br/><a class='item-title'>" + item.name + "</a></div>";
+		return loaded_item;
 	}
 	
 	function load_gallery_item(item) {
-		var gallery_item = "<div id='gallery-item-" + item.id + "' class='item gallery-item ui-corner-all'>";
-		gallery_item += "<p class='gallery-delete' id='gallery-item-delete-" + item.id + "'><a class='ui-icon ui-icon-circle-close'></a></p>"
-		gallery_item += "<img src='" + item.icon + "' /><br/>";
-		gallery_item += "<a class='item-title'>" + item.name + "</a>";
-		gallery_item += "</div>";
-		return gallery_item;
+		return load_item('gallery', item);
 	}
 	
-	function load_media_item(item) {		
-		var media_item = "<div id='media-item-" + item.id + "' class='item media-item ui-corner-all'>";
-		media_item += "<img src='" + item.icon + "' /><br/>";
-		media_item += "<a class='item-title'>" + item.title + "</a>";
-		media_item += "</div>";
-		return media_item;
+	
+	function load_media_item(item) {
+		return load_item('media', item);
 	}
+	
+	// ----------------------------------------
+	// Create temporary item before upload
+	
+	function temp_item(type){
+		var temp_item = "<div id='" + type + "-item-temp' class='item " + type + "-item ui-corner-all'>";
+		temp_item += "<img src='' /><br/><a class='item-title'>...</a></div>";
+		return temp_item;
+	}
+	
+	function temp_gallery_item() {
+		return temp_item('gallery');
+	}
+	
+	function temp_media_item() {
+		return temp_item('media');
+	}
+	
+	// ----------------------------------------
+	// Replace temporary item if upload successful
+	
+	function replace_temp_item(type, new_item) {
+		var temp_item = $('#' + type + '-item-temp');
+		temp_item.attr('id', type + '-item-' + new_item.id);
+		temp_item.child('img').attr('src', new_item.icon);
+		temp_item.child('span.title').html(new_item.title);
+	}
+	
+	function replace_temp_gallery(item) {
+		replace_temp_item('gallery', item);
+	}
+	
+	function replace_temp_media(item) {
+		replace_temp_item('media', item);
+	}
+	
+// ========================================
+// Loading features for main area and tabs
 	
 	// Load main-loading interface
 	function load_gallery(gallery) {
@@ -93,6 +102,40 @@ $(document).ready(function() {
 		});
 	}
 	
+	function display_add_gallery_name(gallery){
+		if (gallery){
+			// Display specific gallery, 
+			// set parent_id and gallery name for "add gallery" form
+			$('input#gallery-parent').attr('value', gallery.id);
+			$('input.gallery-id').attr('value', gallery.id);
+			
+			$('span#gallery-parent-name').parent('p').removeClass('hidden');
+			$('span#gallery-parent-name').text(gallery.name);
+			
+			// Display upload forms
+			if($('#action-upload-solo').attr('style') == "display: none;") {
+				$('#action-upload-solo').show('slide');
+				$('#action-upload-multi').show('slide');
+			}
+			
+		} else {
+			// User want to display main gallery
+			
+			// Hide upload forms
+			if($('#action-upload-solo').attr('style') != "display: none;") {
+				$('#action-upload-solo').hide('slide');
+				$('#action-upload-multi').hide('slide');
+			}
+			
+			// Update gallery id attributes
+			$('input#gallery-parent').attr('value', '');
+			$('input.gallery-id').attr('value', '');
+			
+			// Hide gallery name
+			$('span#gallery-parent-name').parent('p').addClass('hidden');
+		}
+	}
+	
 	function add_or_display_tab(gallery) {
 		
 		// Load new tabs if not already present
@@ -115,6 +158,51 @@ $(document).ready(function() {
 		$('#gallery-tab-' + gallery.id).parent('li').addClass('ui-tabs-selected');
 		$("#tabs").tabs('select', 'gallery-tab-' + gallery.id);
 	}
+	
+// ========================================
+// Ajax Form
+	
+    $('#gallery-add').ajaxForm({
+		dataType:  'json',
+		success: function(data){
+			var gallery = data.galleries[0];
+			$('#main-area > div').append(load_gallery_item(gallery));
+			add_or_display_tab(gallery);
+			$("input#gallery-name").attr("value", "");
+		}
+    });
+
+    $('#media-solo-form').ajaxForm({
+		dataType:  'json',
+		beforeSubmit: function(){
+			// Display temporary gallery in main area displayed div
+			$('#main-area > div').append(load_media_item(data.medias[0]));
+		},
+		success: function(data){
+			// Display result gallery in main area displayed div
+			$('#main-area > div').append(load_media_item(data.medias[0]));
+		}
+    });
+	
+	// ----------------------------------------
+	// Input customization
+	
+	// erasable class is an input containing default text
+	$('.erasable').focus( function() {
+		$(this).attr('value', '');
+		$(this).removeClass('erasable');
+	});
+	
+	$('.erasable').blur( function() {
+		if (!$(this).attr('value')){
+			$(this).attr('value', this.title);
+			$(this).addClass('erasable');
+		}
+	});
+	
+
+// ========================================
+// Actions on DOM
 	
 	$(".gallery-delete a").livequery('click', function() {
 	var gallery_id = $(this).parent('p').get(0).id.split('-').pop();
@@ -151,7 +239,7 @@ $(document).ready(function() {
 		// Display thickbox corresponding to selected item
 	});
 	
-	$(".close-tab").livequery('click', function(event) {
+	$(".gallery-item .close-tab").livequery('click', function(event) {
 		var gallery_id = this.id.split('-').pop();
 		
 		// empty div used by selected tab
@@ -168,43 +256,6 @@ $(document).ready(function() {
 		$("a#galleries-tab").parent('li').addClass('ui-tabs-selected');
 		$("#tabs").tabs('select', 'galleries-tab');
 	});
-	
-// ========================================
-// Ajax Form
-	
-    $('#gallery-add').ajaxForm({
-		dataType:  'json',
-		success: function(data){
-			var gallery = data.galleries[0];
-			$('#main-area > div').append(load_gallery_item(gallery));
-			add_or_display_tab(gallery);
-			$("input#gallery-name").attr("value", "");
-		}
-    });
-
-    $('#media-solo-form').ajaxForm({
-		dataType:  'json',
-		success: function(data){
-			$('#main-area').append(load_media_item(data.medias[0]));
-		}
-    });
-	
-	// ----------------------------------------
-	// Input customization
-	
-	// erasable class is an input containing default text
-	$('.erasable').focus( function() {
-		$(this).attr('value', '');
-		$(this).removeClass('erasable');
-	});
-	
-	$('.erasable').blur( function() {
-		if (!$(this).attr('value')){
-			$(this).attr('value', this.title);
-			$(this).addClass('erasable');
-		}
-	});
-	
 	
 // ========================================
 // Init Script
